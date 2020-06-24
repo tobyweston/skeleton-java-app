0019b89691a7a733ed87d34f81dfc34d88f94714
Toby <toby.weston@gmail.com>
2020-06-24 09:34:29 +0100

    stripe UUID

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,16 +6,18 @@ import java.util.Set;
 
 public class QueryProcessor {
 
-    public static Set<String> cache = new HashSet<>();
-    
-    public String process(String query) throws IOException {
-        if (query == null) {
-            throw new IOException("No query was supplied, perhaps no query parameter was supplied on the URI (q= instead of q=param)");
-        }
-        cache.add(query.substring(9));
-        if (query.toLowerCase().contains("romeo and juliet")) {
-            return "William Shakespeare";
-        }
-        return "Unknown question";
-    }
+	public static Set<String> cache = new HashSet<>();
+
+	public String process(String query) throws IOException {
+		if (query == null) {
+			throw new IOException("No query was supplied, perhaps no query parameter was supplied on the URI (q= instead of q=param)");
+		}
+		cache.add(query.substring(9));
+		if (query.toLowerCase().contains("romeo and juliet"))
+			return "William Shakespeare";
+
+		if (query.toLowerCase().contains("what is your name"))
+			return "Toby";
+		return "Unknown question";
+	}
 }
