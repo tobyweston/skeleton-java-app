4f4d72b7117caca8677988ce1558afae8438c8b3
Toby <toby.weston@gmail.com>
2020-06-24 08:31:24 +0100

    adding a cache

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,13 +1,18 @@
 package com.develogical;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class QueryProcessor {
 
+    public static Set<String> cache = new HashSet<>();
+    
     public String process(String query) throws IOException {
         if (query == null) {
             throw new IOException("No query was supplied, perhaps no query parameter was supplied on the URI (q= instead of q=param)");
         }
+        cache.add(query);
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
