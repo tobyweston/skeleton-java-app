ca5cf2972ca7f9aae5ce703b43971bc8080a484f
Toby <toby.weston@gmail.com>
2020-06-24 10:49:32 +0100

    shhhh

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,29 +1,38 @@
 package com.develogical;
 
+import simplehttp.HttpClients;
+import simplehttp.HttpResponse;
+import simplehttp.Url;
+
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.Set;
 
 public class QueryProcessor {
 
-    public static Set<String> cache = new HashSet<>();
-    
-    public String process(String query) throws IOException {
-        if (query == null) {
-            throw new IOException("No query was supplied, perhaps no query parameter was supplied on the URI (q= instead of q=param)");
-        }
-        cache.add(query);
-        if (query.toLowerCase().contains("romeo and juliet")) 
-            return "William Shakespeare";
-        if (query.toLowerCase().contains("what is your name"))
+	public static Set<String> cache = new HashSet<>();
+
+	public String process(String query) throws IOException {
+		if (query == null) {
+			throw new IOException("No query was supplied, perhaps no query parameter was supplied on the URI (q= instead of q=param)");
+		}
+		cache.add(query);
+		if (query.toLowerCase().contains("romeo and juliet"))
+			return "William Shakespeare";
+		if (query.toLowerCase().contains("what is your name"))
 			return "Toby";
-        if (query.toLowerCase().contains("what is 11 plus 18"))
+		if (query.toLowerCase().contains("what is 11 plus 18"))
 			return "29";
-        if (query.toLowerCase().contains("which of the following numbers is the largest: ")) {
+		if (query.toLowerCase().contains("which of the following numbers is the largest: ")) {
 			new LargestNumber(query).is();
-        }
-        if (query.toLowerCase().contains("which city is the Eiffel tower in"))
-            return "Paris";
-        return "Unknown question";
-    }
+		}
+		if (query.toLowerCase().contains("which city is the Eiffel tower in"))
+			return "Paris";
+
+		String question = query.substring(query.indexOf('=') + 1) ;
+		String url = "https://mysterious-reaches-24898.herokuapp.com/api/?q=" + question;
+		HttpResponse response = HttpClients.anApacheClient().get(Url.url(url));
+		return response.getContent().asString();
+	}
 }
--- src/test/java/com/develogical/SquareAndCubeTest.java
+++ src/test/java/com/develogical/SquareAndCubeTest.java
@@ -14,4 +14,10 @@ public class SquareAndCubeTest {
 		String query = "50c9a0c0: which of the following numbers is both a square and a cube: 729, 1681, 381, 881";
 		assertThat(new SquareAndCute(query).result(), is(""));
 	}
+	
+	@Test
+	public void substring() {
+		String query = "https://mysterious-reaches-24898.herokuapp.com/api/?=abc";
+		assertThat(query.substring(query.indexOf('=') + 1), is("abc"));
+	}
 }
